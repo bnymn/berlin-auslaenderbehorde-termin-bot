0ea0d9a426bef6139e971b9afa6971793847550c
Bunyamin Inan <inanbunyamin90@gmail.com>
2024-06-22 08:52:38 +0200

    selenium hub and multiple nodes

--- src/main/java/com/yilmaznaslan/utils/DriverUtils.java
+++ src/main/java/com/yilmaznaslan/utils/DriverUtils.java
@@ -111,13 +111,8 @@ public class DriverUtils {
         // Add a fake user-agent to make it look like a regular browser
         options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
 
-        options.setCapability("unexpectedAlertBehavior", "accept");
-
-        Map<String, Object> cloudOptions = new HashMap<>();
-        cloudOptions.put("unexpectedAlertBehavior", "accept");
-
-        //options.setCapability("cloud:options", cloudOptions);
-
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+        options.setCapability("nodename:applicationName", "chrome_customer1");
 
         return options;
     }
